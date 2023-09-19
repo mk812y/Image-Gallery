00dea6c823acbe19be6ccbf747d053c6c0896042
mk812y <mk812y@gmail.com>
2023-09-19 19:05:15 +0200

    Initial Commit

---
+++ Image Gallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image Gallery
+//
+//  Created by Михаил Куприянов on 19.9.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image Gallery/Image_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_GalleryApp.swift
+//  Image Gallery
+//
+//  Created by Михаил Куприянов on 19.9.23..
+//
+
+import SwiftUI
+
+@main
+struct Image_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
